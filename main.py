d063c4efc0c322cca42467f8188e6fd7e46dec99
ttmje <ivanttmyurlov@gmail.com>
2022-07-18 21:54:29 +0300

    added menu, fixin code

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ class Client():
 
     API_KEY = "6d8b6d8be60c37121c60ea2b26134d2a7fd960832464c9db5857c57d47a8f46c"
     API_SECRET = "8555db4d12b17ea2ece76768841f931edf12672806dfe0b87de85e7b9796cfe5"
-    timestamp = int(time.time()*1000)
     symbol = 'BTCUSDT'
 
     def check_con(self):
@@ -19,7 +18,8 @@ class Client():
             print("Success", time.text)
 
     def get_open_orders(self):
-        self.querystring = urlencode({'timestamp': self.timestamp, 'recvWindow': 10000})
+        self.timestamp = int(time.time() * 1000)
+        self.querystring = urlencode({'timestamp': self.timestamp, 'recvWindow': 15000})
         self.key = hmac.new(self.API_SECRET.encode('utf-8'), self.querystring.encode('utf-8'),digestmod=hashlib.sha256).hexdigest()
         headers = {'Content-type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': self.API_KEY}
         url = f'https://testnet.binancefuture.com/fapi/v1/openOrders?{self.querystring}&signature={self.key}'
@@ -30,36 +30,47 @@ class Client():
             self.tickers.append(json.loads(self.orders.text)[i]['symbol'])
         print('Open orders count:', self.count, 'tickers is:', *self.tickers)
 
-
-
-    def close_all_pos(self):
+    def close_pos(self):
             if self.count != 0:
                 user_symbol = input('Type ticker for cancel or press [ENTER] to cancel ALL open orders: ')
                 if user_symbol != '':
                     self.symbol = user_symbol
-                    self.querystring = urlencode({'symbol': self.symbol, 'timestamp': self.timestamp, 'recvWindow': 10000})
-                    self.key = hmac.new(self.API_SECRET.encode('utf-8'), self.querystring.encode('utf-8'),digestmod=hashlib.sha256).hexdigest()
-                    headers = {'Content-type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': self.API_KEY}
-                    url = f'https://testnet.binancefuture.com/fapi/v1/allOpenOrders?{self.querystring}&signature={self.key}'
-                    r = requests.delete(url, headers=headers)
-                    print(url)
-                    print(r.text)
+                    task.urlgen()
+                    self.symbol = []
                 else:
                     for i in range(len(self.tickers)):
                         self.symbol = self.tickers[i]
-                        self.querystring = urlencode({'symbol': self.symbol, 'timestamp': self.timestamp, 'recvWindow': 10000})
-                        self.key = hmac.new(self.API_SECRET.encode('utf-8'), self.querystring.encode('utf-8'),digestmod=hashlib.sha256).hexdigest()
-                        headers = {'Content-type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': self.API_KEY}
-                        url = f'https://testnet.binancefuture.com/fapi/v1/allOpenOrders?{self.querystring}&signature={self.key}'
-                        r = requests.delete(url, headers=headers)
-                        print(url)
-                        print(r.text)
+                        task.urlgen()
+                        self.symbol = []
             else:
                 print('[There is no open orders to cancel]')
+
+    def urlgen(self):
+        self.timestamp = int(time.time() * 1000)
+        self.querystring = urlencode({'symbol': self.symbol, 'timestamp': self.timestamp, 'recvWindow': 10000})
+        self.key = hmac.new(self.API_SECRET.encode('utf-8'), self.querystring.encode('utf-8'),digestmod=hashlib.sha256).hexdigest()
+        headers = {'Content-type': 'application/x-www-form-urlencoded', 'X-MBX-APIKEY': self.API_KEY}
+        url = f'https://testnet.binancefuture.com/fapi/v1/allOpenOrders?{self.querystring}&signature={self.key}'
+        r = requests.delete(url, headers=headers)
+        self.symbol = []
+        print(url)
+        print(r.text)
+
+    def menu(self):
+        cmd = input('Type command: ')
+        while True:
+            if cmd == '1':
+                task.check_con()
+            elif cmd == '2':
+                task.get_open_orders()
+            elif cmd == '3':
+                task.close_pos()
+            else:
+                print('Wrong command!')
+            cmd = input('Type command: ')
+        return
 
 
 if __name__ == '__main__':
     task = Client()
-    task.check_con()
-    task.get_open_orders()
-    task.close_all_pos()
+    task.menu()
