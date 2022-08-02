dea30c9015aa88ef7fa334869daa7c01bf2caa40
ttmje <ivanttmyurlov@gmail.com>
2022-08-02 20:27:42 +0300

    works almost as planned

--- main.py
+++ main.py
@@ -4,10 +4,38 @@ import requests
 import json
 import hmac, hashlib
 import time
+from flask import Flask, render_template, request, flash
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'segwergergwergwergwergwer5gy34525345'
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('get_open_orders'):
+           if task.get_open_orders() == []:
+              flash(f"Open orders: 0")
+           else:
+              flash(f"Open orders: {task.get_open_orders()}")
+           return render_template("index.html")
+
+        elif request.form.get('close_all_orders'):
+            flash(f'Closed: {task.close_pos()} Open: {task.get_orders_count()}')
+            return render_template("index.html")
+
+        elif request.form.get('close_one'):
+            try:
+                user_symbol = request.form.get('ticker')
+                if len(user_symbol) <= 7:
+                    flash(f'Closed: {task.close_one(user_symbol)} Open: {task.get_orders_count()}')
+                    return render_template("index.html")
+                else:
+                    return render_template("index.html")
+            except:
+                return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 class Client():
-
     API_KEY = "6d8b6d8be60c37121c60ea2b26134d2a7fd960832464c9db5857c57d47a8f46c"
     API_SECRET = "8555db4d12b17ea2ece76768841f931edf12672806dfe0b87de85e7b9796cfe5"
     symbol = 'BTCUSDT'
@@ -30,35 +58,44 @@ class Client():
         for i in range(len(json.loads(self.orders.text))):
             self.tickers.append(json.loads(self.orders.text)[i]['symbol'])
         print('Open orders count:', self.count, ', tickers is:', *self.tickers)
+        return self.tickers
+
 
     def close_pos(self):
         try:
+            for i in range(len(self.tickers)):
+                self.symbol = self.tickers[i]
+                task.urlgen()
+                self.symbol = []
+            if json.loads(self.r.text)['code'] == 200:
+                print('Closed:', len(self.tickers), 'Open:', task.get_orders_count())
+                b = self.tickers
+            else:
+                errors = urlparse(self.url)
+                err = (parse_qs(errors[4]))
+                tkr = err["symbol"]
+                itemstoremove = set(err["symbol"])
+                b = [x for x in self.tickers if x not in itemstoremove]
+                print(f'Error! {len(tkr)}, Closed: {len(b)}, Open:, {task.get_orders_count()}')
+        except:
+            print('Orders not found, get open orders')
+        return len(b)
+
+    def close_one(self, user_symbol):
+        try:
             if self.count != 0:
-                user_symbol = input('Type ticker for cancel or press [ENTER] to cancel ALL open orders: ')
+                user_symbol = request.form.get('ticker')
                 if user_symbol != '':
                     self.symbol = user_symbol
                     task.urlgen()
                     self.tickers.remove(user_symbol.upper())
-                    print('Closed: 1', 'Open:', task.get_orders_count())
                 else:
-                    self.tickers.append('BTTTTTTCUSDT')
-                    for i in range(len(self.tickers)):
-                        self.symbol = self.tickers[i]
-                        task.urlgen()
-                        self.symbol = []
-                    if json.loads(self.r.text)['code'] == 200:
-                        print('Closed:', len(self.tickers), 'Open:', task.get_orders_count())
-                    else:
-                        errors = urlparse(self.url)
-                        err = (parse_qs(errors[4]))
-                        tkr = err["symbol"]
-                        itemstoremove = set(err["symbol"])
-                        b = [x for x in self.tickers if x not in itemstoremove]
-                        print(f'Error! {tkr}, Closed: {len(b)}, Open:, {task.get_orders_count()}')
-            else:
-                print('[There is no open orders to cancel]')
+                    print('There is no orders to cancel')
         except:
-            print('Orders not found, get open orders')
+            print('Error')
+            return "error, wrong ticker"
+        return user_symbol
+
 
     def get_orders_count(self):
         self.timestamp = int(time.time() * 1000)
@@ -109,4 +146,8 @@ class Client():
 
 if __name__ == '__main__':
     task = Client()
+    app.run(debug=False)
     task.menu()
+
+
+
