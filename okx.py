c55df1b90c5a797cbad0d1c0b3c826e9046ba20d
ttmje <ivanttmyurlov@gmail.com>
2022-08-09 22:00:39 +0300

    testing post

--- okx.py
+++ okx.py
@@ -3,6 +3,8 @@ import hashlib
 import hmac
 import json
 import datetime
+import urllib
+from urllib.parse import urlencode
 
 import requests
 
@@ -18,22 +20,50 @@ class Client_okx():
         timestamp = now.isoformat("T", "milliseconds")
         return timestamp + "Z"
 
-    def get_open_orders(self):
-            time = okx.get_timestamp()
-            print(time)
-            self.prehash = time + "GET" + "/api/v5/trade/orders-pending" + ''
-            self.key = hmac.new(self.API_SECRET.encode('utf-8'), self.prehash.encode('utf-8'), digestmod=hashlib.sha256).digest()
-            self.encoded = base64.b64encode(self.key)
-            self.headers = {'Content-Type':'application/json',
-                            'OK-ACCESS-KEY':self.API_KEY,
-                            'OK-ACCESS-SIGN':self.encoded,
-                            'OK-ACCESS-TIMESTAMP':time,
-                            'OK-ACCESS-PASSPHRASE':self.PASSPHRASE,
-                            'x-simulated-trading': '1'}
-            url = 'https://www.okx.com/api/v5/trade/orders-pending'
-            self.orders = requests.get(url, headers=self.headers)
-            print(self.orders.text)
+    def sign(self, timestamp, method, endpoint, body=''):
+        if str(body) == '{}' or str(body) == 'None':
+            body = ''
+        prehash = timestamp + method + endpoint + str(body)
+        key = hmac.new(self.API_SECRET.encode('utf-8'), prehash.encode('utf-8'), digestmod=hashlib.sha256).digest()
+        encoded = base64.b64encode(key)
+        return encoded
 
+    def get_headers(self, sign, timestamp):
+        headers = {'Content-Type': 'application/json',
+                        'OK-ACCESS-KEY': self.API_KEY,
+                        'OK-ACCESS-SIGN': sign,
+                        'OK-ACCESS-TIMESTAMP': timestamp,
+                        'OK-ACCESS-PASSPHRASE': self.PASSPHRASE,
+                        'x-simulated-trading': '1',
+                        }
+        return headers
+
+    def get_open_orders(self):
+            timestamp = okx.get_timestamp()
+            url = 'https://www.okx.com/api/v5/trade/orders-pending'
+            headers = okx.get_headers((okx.sign(timestamp, "GET", "/api/v5/trade/orders-pending", '')), timestamp)
+            self.orders = requests.get(url, headers=headers)
+            to_json = json.loads(self.orders.text)['data']
+            self.tickers = []
+            for i in range(len(to_json)):
+                self.tickers.append(to_json[i]['instId'])
+            print(*self.tickers)
+            okx.close_pos()
+
+    def close_pos(self):
+            for i in range(len(self.tickers)):
+                timestamp = okx.get_timestamp()
+                self.symbol = self.tickers[i]
+                params = urlencode({'instId': self.symbol})
+                base_url = 'https://www.okx.com'
+                request_path = f'/api/v5/trade/cancel-batch-orders?{params}'
+                url = base_url + request_path
+                headers = okx.get_headers((okx.sign(timestamp, 'POST', request_path, params)), timestamp)
+                canceled = requests.post(url, data=params, headers=headers)
+                print(canceled.request.url)
+                print(canceled.request.body)
+                print(canceled.request.headers)
+                print(canceled.text)
 
 
 if __name__ == '__main__':
